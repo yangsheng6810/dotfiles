b66185ffa9f9592b9f3e863a87b73745979d5eb0
Yang Sheng <yangsheng6810@gmail.com>
2016-07-01 09:47:22 -0400

    Fix completion problem in oh-my-zsh
    Fix case sensitive problem in oh-my-zsh

--- .oh-my-zsh/custom/completion.zsh
+++ .oh-my-zsh/custom/completion.zsh
@@ -1,15 +1,5 @@
-set CASE_SENSITIVE
+## case-insensitive (all),partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
 autoload bashcompinit
 bashcompinit
- ## case-insensitive (all),partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  if [ "x$HYPHEN_INSENSITIVE" = "xtrue" ]; then
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
-    unset HYPHEN_INSENSITIVE
-  else
-    zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-  fi
-fi
+set CASE_SENSITIVE
