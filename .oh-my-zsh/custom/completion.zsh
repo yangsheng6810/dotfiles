a8d655aca2a9270d489628653175992f3d99fcf8
Yang Sheng <yangsheng6810@gmail.com>
2016-07-01 12:51:00 -0400

    Fix case sensitivity for zsh

--- .oh-my-zsh/custom/completion.zsh
+++ .oh-my-zsh/custom/completion.zsh
@@ -1,5 +1,5 @@
 ## case-insensitive (all),partial-word and then substring completion
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list 'r:|=*' 'l:|=* r:|=*'
 autoload bashcompinit
 bashcompinit
 set CASE_SENSITIVE
