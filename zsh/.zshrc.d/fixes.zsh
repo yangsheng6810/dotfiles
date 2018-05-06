1e1c228fba8fa62b37a696a5e65b0cac3f52985e
Yang Sheng <yangsheng6810@gmail.com>
2018-05-06 18:29:58 +0000

    Add remote directory tracking for eterm

--- zsh/.zshrc.d/fixes.zsh
+++ zsh/.zshrc.d/fixes.zsh
@@ -33,6 +33,18 @@ function zle-line-finish () {
     fi
 }
 
+if [ "$TERM" = "eterm-256color" ]; then
+    function precmd() {
+    echo -e "\033AnSiTc" "$(pwd)"
+
+    ## "farm" is the name of the server in my .ssh/config
+    ## replace it with your server's name, or with $(hostname -f)
+    ## you may also want to add 'echo -e "\033AnSiTu" "$LOGNAME"'
+    echo -e "\033AnSiTh" $(hostname)
+    echo -e "\033AnSiTu" "$LOGNAME"
+    }
+fi
+
 # Fix PS1 battery indicator problem when we incorrectly have
 # more than one batteries
 # Sample problematic acpi output:
