3f0b8f87eb8961c2619cac769111fed1ca30d7b1
Yang Sheng <yangsheng6810@gmail.com>
2018-07-18 22:44:57 -0400

    Remove pacman --color=auto from T420 specific zsh config

--- zsh/.zshrc.d/machines/t420.zsh
+++ zsh/.zshrc.d/machines/t420.zsh
@@ -1,7 +1,6 @@
 alias addon-sdk="cd /opt/addon-sdk && source bin/activate; cd -"
 alias tllocalmgr="tllocalmgr --mirror='http://mirrors.tuna.tsinghua.edu.cn/CTAN/systems/texlive/tlnet'"
 
-alias pacman='pacman --color=auto'
 TEXMFMAIN="/usr/share/texmf-dist/"
 alias 'git_latexdiff'="git latexdiff --bibtex --ignore-latex-errors --latexdiff-flatten"
 
