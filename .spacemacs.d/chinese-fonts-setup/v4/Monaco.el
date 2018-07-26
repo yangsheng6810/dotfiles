2fa0369134fd4e7f2544fd2f3ad7a09b0da4814c
Yang Sheng <yangsheng6810@gmail.com>
2018-07-26 01:43:53 -0700

    Upcate cnfonts Monaco profile

--- .spacemacs.d/chinese-fonts-setup/v4/Monaco.el
+++ .spacemacs.d/chinese-fonts-setup/v4/Monaco.el
@@ -4,8 +4,8 @@
 ;;; `cnfonts-insert-fontname’ 来选择一个 *可用* 字体，然后在当前光标处插入其字体名称。
 (setq cnfonts--custom-set-fontnames
       '(
-        ("Monaco" "Fira Code" "AnonymousPro" "Fantasque Sans Mono" "Anonymous Pro" "Source Code Pro" "Liberation Mono" "Ubuntu Mono" "Courier New" "DejaVu Sans Mono" "Droid Sans Mono" "Consolas" "PragmataPro" "Courier" "MonacoB" "MonacoB2" "MonacoBSemi" "Droid Sans Mono Pro" "Inconsolata" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Hack" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Menlof" "Cousine" "Fira Mono" "Lekton" "M+ 1mn" "BPmono" "Free Mono" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT" "Iosevka Term" "Inconsolata-dz" "American Typewriter" "Menlo" "Anka/Coder Condensed" "M+ 1m" "CamingoCode" "Office Code Pro" "Roboto Mono" "Input Mono" "Courier Prime Code" "NanumGothicCoding" "Monoid" "Edlo" "Iosevka" "Mononoki" "Robot Mono" "Fantasque" "Go Mono" "Noto Sans Mono CJK" "InputMonoCompressed" "Hasklig" "Terminus" "FantasqueSansMono" "3270" "Arimo" "D2Coding" "Inconsolata-g" "ProFont for Powerline" "Meslo" "Meslo Dotted" "Noto Mono" "Symbol Neu" "Tinos" "Space Mono")
-        ("思源黑体 CN")
+        ("Monaco" "Fira Code" "AnonymousPro" "Fantasque Sans Mono" "Anonymous Pro" "Source Code Pro" "Liberation Mono" "Ubuntu Mono" "Courier New" "DejaVu Sans Mono" "Droid Sans Mono" "Consolas" "PragmataPro" "Courier" "MonacoB" "MonacoB2" "MonacoBSemi" "Droid Sans Mono Pro" "Inconsolata" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Hack" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Menlof" "Cousine" "Fira Mono" "Lekton" "M+ 1mn" "BPmono" "Free Mono" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT" "Iosevka Term" "Inconsolata-dz" "American Typewriter" "Menlo" "Anka/Coder Condensed" "M+ 1m" "CamingoCode" "Office Code Pro" "Roboto Mono" "Input Mono" "Courier Prime Code" "NanumGothicCoding" "Monoid" "Edlo" "Iosevka" "Mononoki" "Robot Mono" "Fantasque" "Go Mono" "Noto Sans Mono CJK" "InputMonoCompressed" "Hasklig" "Terminus" "FantasqueSansMono" "3270" "Arimo" "D2Coding" "Inconsolata-g" "ProFont for Powerline" "Meslo" "Meslo Dotted" "Noto Mono" "Symbol Neu" "Tinos" "Space Mono" "Ubuntu\n      Mono" "Noto Sans\n      Mono CJK")
+        ("WenQuanYi Micro Hei Mono" "思源黑体 CN" "WenQuanYi Zen Hei Mono" "文泉驿等宽微米黑" "文泉驿等宽正黑" "Source Han Sans CN" "Source Han Serif CN" "Source Han Sans" "Source Han Serif" "Noto Sans MOno CJK SC" "微软雅黑" "Microsoft Yahei" "Ubuntu Mono" "黑体" "SimHei" "宋体" "SimSun" "新宋体" "NSimSun" "Hiragino Sans GB")
         ("HanaMinB" "SimSun-ExtB" "MingLiU-ExtB" "PMingLiU-ExtB" "MingLiU_HKSCS-ExtB")
         ))
 
@@ -14,19 +14,19 @@
 ;;; 按 C-<up> 增大光标处字号，按 C-<down> 减小光标处字号。
 (setq cnfonts--custom-set-fontsizes
       '(
-        (9    12.0 12.0)
-        (10   13.5 13.5)
-        (11.5 15.0 15.0)
-        (12.5 16.5 16.5)
-        (14   18.0 18.0)
-        (15   19.5 19.5)
-        (16   21.0 21.0)
-        (18   24.0 24.0)
-        (20   26.5 26.5)
-        (22   28.5 28.5)
-        (24   31.5 31.5)
-        (26   34.0 34.0)
-        (28   37.0 37.0)
-        (30   40.0 40.0)
-        (32   42.0 42.0)
+        (9    10.5 10.5)
+        (10   12.0 12.0)
+        (11   14.0 14.0)
+        (12.5 15.0 15.0)
+        (14   16.5 16.5)
+        (15   18.0 18.0)
+        (16   19.5 19.5)
+        (18   22.0 22.0)
+        (20   24.0 24.0)
+        (22   26.5 26.5)
+        (24   28.5 28.5)
+        (26   31.0 31.0)
+        (28   33.0 33.0)
+        (30   36.0 36.0)
+        (32   38.5 38.5)
         ))
