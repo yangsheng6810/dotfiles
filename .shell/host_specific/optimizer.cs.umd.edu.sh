68b2cb5565ebdf6f30f6a7c3de04355b867f1a05
Yang Sheng <yangsheng6810@gmail.com>
2018-11-26 16:09:01 -0500

    Update PATH for optimizer

--- .shell/host_specific/optimizer.cs.umd.edu.sh
+++ .shell/host_specific/optimizer.cs.umd.edu.sh
@@ -4,3 +4,9 @@ LC_ALL=""
 LANG="en_US.UTF-8"
 LC_COLLATE="C"
 TERM="xterm-256color"
+# added by Anaconda3 installer
+export PATH="/fs/optimizer/styang/anaconda3/bin:$PATH"
+
+# for gurobi
+export GRB_LICENSE_FILE="$XDG_CONFIG_HOME/gurobi.lic"
+PATH="${PATH}:${GUROBI_HOME}/bin"
