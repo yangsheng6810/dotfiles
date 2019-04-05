8e7aa65ff2a674f0df14b2729b20e377d3236c6d
Sheng Yang <yangsheng6810@gmail.com>
2019-04-05 10:51:10 -0400

    Update script to parse novel from html page

--- bin/parse_novel_from_html.py
+++ bin/parse_novel_from_html.py
@@ -7,12 +7,29 @@ input_file = sys.argv[1]
 with open(input_file, "r") as i_file:
     content = i_file.read()
 
-soup = BeautifulSoup(content)
-title = soup.find_all("h1")[0].text
-# print(title)
-body = soup.find_all("div", {"id":"content"})[0].text
+soup = BeautifulSoup(content, features="lxml")
+for br in soup.find_all("br"):
+    br.replace_with("\n")
+title_find = soup.find_all("h3")
+if not title_find:
+    title_find = soup.find_all("h1")
+title = title_find[0].text
+
+body_find = soup.find_all("p", {"class":"articlecontent"})
+if not body_find:
+    body_find = soup.find_all("div", {"id":"_chapter"})
+if not body_find:
+    body_find = soup.find_all("div", {"id":"nr1"})
+if not body_find:
+    body_find = soup.find_all("div", {"id":"htmlContent"})
+if not body_find:
+    body_find = soup.find_all("div", {"id":"content"})
+body = body_find[0].text
 
 output_file = "out_" + input_file
 # output_file = title + ".txt"
 with open(output_file, "w") as o_file:
+    print("-"*30, file=o_file)
+    print(title, file=o_file)
+    print("-"*30, file=o_file)
     print(body, file=o_file)
