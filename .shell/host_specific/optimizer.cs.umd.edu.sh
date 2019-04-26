6546fe0a0cd6c6da44e78e7be8f915c39d8deff4
Sheng Yang <yangsheng6810@gmail.com>
2019-04-26 05:42:12 +0000

    Fix gurobi settings for optimizer

--- .shell/host_specific/optimizer.cs.umd.edu.sh
+++ .shell/host_specific/optimizer.cs.umd.edu.sh
@@ -8,5 +8,7 @@ TERM="xterm-256color"
 export PATH="/fs/optimizer/styang/anaconda3/bin:$PATH"
 
 # for gurobi
+export GUROBI_HOME="$HOME/gurobi/gurobi811/linux64"
+export PATH="${PATH}:${GUROBI_HOME}/bin"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${GUROBI_HOME}/lib"
 export GRB_LICENSE_FILE="$XDG_CONFIG_HOME/gurobi.lic"
-PATH="${PATH}:${GUROBI_HOME}/bin"
