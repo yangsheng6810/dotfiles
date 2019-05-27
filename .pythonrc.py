5dd48689719dbde97db53e68db12ae6cb193cfad
Sheng Yang <yangsheng6810@gmail.com>
2019-05-26 21:16:38 -0400

    Only apply pythonrc when interactive

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,19 +1,22 @@
-from math import *
-import scipy as sp
-import numpy as np
-import matplotlib.pyplot as plt
+import sys
 
-# import jedi for completion
-try:
-    from jedi.utils import setup_readline
-    setup_readline()
-except ImportError:
-    # Fallback to the stdlib readline completer if it is installed.
-    # Taken from http://docs.python.org/2/library/rlcompleter.html
-    print("Jedi is not installed, falling back to readline")
+if sys.__stdin__.isatty():
+    from math import *
+    import scipy as sp
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    # import jedi for completion
     try:
-        import readline
-        import rlcompleter
-        readline.parse_and_bind("tab: complete")
+        from jedi.utils import setup_readline
+        setup_readline()
     except ImportError:
-        print("Readline is not installed either. No tab completion is enabled.")
+        # Fallback to the stdlib readline completer if it is installed.
+        # Taken from http://docs.python.org/2/library/rlcompleter.html
+        print("Jedi is not installed, falling back to readline")
+        try:
+            import readline
+            import rlcompleter
+            readline.parse_and_bind("tab: complete")
+        except ImportError:
+            print("Readline is not installed either. No tab completion is enabled.")
