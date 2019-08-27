310512eaadac75b6d9e11128f096a6f0b8b45c95
Sheng Yang <yangsheng6810@gmail.com>
2019-08-27 20:49:44 +0000

    Fix utf-8 in optimizer

--- .shell/host_specific/optimizer.cs.umd.edu.sh
+++ .shell/host_specific/optimizer.cs.umd.edu.sh
@@ -1,7 +1,7 @@
 alias suroot="sudo -E -s"
 export PYTHONSTARTUP="$(python -m jedi repl)"
 LC_ALL=""
-LANG="en_US.UTF-8"
+export LANG="en_US.UTF-8"
 LC_COLLATE="C"
 # TERM="xterm-256color"
 # added by Anaconda3 installer
