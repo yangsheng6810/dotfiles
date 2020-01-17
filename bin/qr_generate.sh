1ece9417ef486fe8f7b40a2845eaf8b607f7db48
Sheng Yang <yangsheng6810@gmail.com>
2020-01-17 15:55:25 -0600

    Tweak qr_generate code

--- bin/qr_generate.sh
+++ bin/qr_generate.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Usage: $0 <input> <output_prefix>"
 else
     if [ -f $FILENAME ]; then
-        split -C 1000 "$FILENAME" qr_generate_temp_
+        split -C 2000 "$FILENAME" qr_generate_temp_
         for file in qr_generate_temp_*; do
             <"$file" qrencode -s 3 -d 150 -o "${OUTPUT_PREFIX}_${file#qr_generate_temp_}".qr
             rm "${file}"
