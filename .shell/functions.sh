0aa4c0689755495446cbb8a60f221b78777848c1
Sheng Yang <yangsheng6810@gmail.com>
2020-07-02 00:09:38 -0500

    Update functions for shell

--- .shell/functions.sh
+++ .shell/functions.sh
@@ -12,3 +12,36 @@ function stopwatch(){
         sleep 0.1
     done
 }
+
+function printdate(){
+    echo $(date +"%Y-%m-%d")
+}
+
+function rebuild_modules(){
+    dkms autoinstall -k $(uname -r)
+}
+
+function ssh_tunnel(){
+    # ssh -2qTfnN -D 7070 $1
+    ssh -2qTN -D 7070 $1
+}
+
+function myip() {
+    echo >&2 "Getting your public IPv6 address"
+    if type wget >/dev/null; then
+        wget -qO- 'http://ipv4.icanhazip.com'
+    elif type curl >/dev/null; then
+        curl 'http://ipv4.icanhazip.com'
+    else
+        echo >&2 "Neither of wget and curl found. Install one of them. Abort."
+        exit 1
+    fi
+}
+
+function myip_legacy () {
+    dig +short myip.opendns.com @resolver1.opendns.com
+}
+
+function jpeg2pdf () {
+    convert -background white -page a4 $@
+}
