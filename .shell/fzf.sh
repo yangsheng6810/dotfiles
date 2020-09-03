521ced0efcf753ef49f3ff76c7ab855825feea4a
Sheng Yang <yangsheng6810@gmail.com>
2020-09-03 08:50:26 +0000

    Update .shell/fzf.sh

--- .shell/fzf.sh
+++ .shell/fzf.sh
@@ -1,26 +1,21 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
 FZF_COMPLETE=""
 FZF_KEY_BINDINGS=""
 case $SHELL in
     *bash)
-        FZF_COMPLETE="~/bin/.fzf/shell/completion.bash"
-        FZF_KEY_BINDINGS="~/bin/.fzf/shell/key-bindings.bash"
-        echo "Prepare fzf for bash"
+        FZF_COMPLETE="${HOME}/bin/.fzf/shell/completion.bash"
+        FZF_KEY_BINDINGS="${HOME}/bin/.fzf/shell/key-bindings.bash"
         ;;
     *zsh)
-        FZF_COMPLETE="~/bin/.fzf/shell/completion.zsh"
-        FZF_KEY_BINDINGS="~/bin/.fzf/shell/key-bindings.zsh"
-        echo "Prepare fzf for zsh"
+        FZF_COMPLETE="${HOME}/bin/.fzf/shell/completion.zsh"
+        FZF_KEY_BINDINGS="${HOME}/bin/.fzf/shell/key-bindings.zsh"
         ;;
     *fish)
-        FZF_KEY_BINDINGS="~/bin/.fzf/shell/key-bindings.fish"
+        FZF_KEY_BINDINGS="${HOME}/bin/.fzf/shell/key-bindings.fish"
         ;;
     *)
         echo "Unsupported shell"
         ;;
 esac
 
-[ -f "$FZF_COMPLETE" ] && [[ $- == *i* ]] && source "$FZF_COMPLETE" 2> /dev/null
-[ -f "$FZF_KEY_BINDINGS" ] && source "$FZF_KEY_BINDINGS"
+[ -f "$FZF_COMPLETE" ] && [[ $- == *i* ]] && . "${FZF_COMPLETE}" 2> /dev/null
+[ -f "$FZF_KEY_BINDINGS" ] && . "${FZF_KEY_BINDINGS}"
