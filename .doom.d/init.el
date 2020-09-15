7e3ec9df0eda407ef227cba7fa1e85f890d65b8c
Sheng Yang <yangsheng6810@gmail.com>
2020-09-15 15:38:31 -0500

    Do not use Doom's word-wrap

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -65,9 +65,9 @@
        multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
-       rotate-text       ; cycle region at point between text candidates
-       snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       rotate-text               ; cycle region at point between text candidates
+       snippets                  ; my elves. They type so I don't have to
+       ;; word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        dired             ; making dired pretty [functional]
