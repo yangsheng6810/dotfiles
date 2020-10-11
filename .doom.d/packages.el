1c7eb1addc842d6d257f8892305ba480d30db548
Sheng Yang <yangsheng6810@gmail.com>
2020-10-11 11:27:54 -0500

    Start using org-roam-bibtex

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -92,3 +92,12 @@
 
 (package! flycheck-package)
 (package! chinese-yasdcv)
+;; org-roam-bibtex
+(package! org-roam-bibtex
+  :recipe (:host github :repo "org-roam/org-roam-bibtex"))
+
+;; When using org-roam via the `+roam` flag
+(unpin! org-roam company-org-roam)
+
+;; When using bibtex-completion via the `biblio` module
+(unpin! bibtex-completion helm-bibtex ivy-bibtex)
