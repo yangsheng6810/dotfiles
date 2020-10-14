2dc5d76e5f642a1d07e0594b3de7b7876d871911
Sheng Yang <yangsheng6810@gmail.com>
2020-10-14 11:57:09 +0000

    Fix typo

--- .shell/pre-fix.sh
+++ .shell/pre-fix.sh
@@ -1,7 +1,7 @@
 # This file contains fixes that need to be done before zgen is loaded
 
 # Prepare FZF_BASE for fzf to use on optimizer
-if [ "$(hostname)" = "optimizer.cs.umd.edu.sh" ]; then
-    export PATH="${HOME}/bin/binary:$PATH"
+if [ "$(hostname)" = "optimizer.cs.umd.edu" ]; then
+    export PATH="${HOME}/bin:$PATH"
     export FZF_BASE="${HOME}/bin/.fzf"
 fi
