8dda5b300b8cde114205d377ac6abb16949402ba
Sheng Yang <yangsheng6810@gmail.com>
2020-10-14 06:59:28 -0500

    Fix fzf loading on osprey and optimizer

--- .shell/pre-fix.sh
+++ .shell/pre-fix.sh
@@ -1,7 +1,7 @@
 # This file contains fixes that need to be done before zgen is loaded
 
 # Prepare FZF_BASE for fzf to use on optimizer
-if [ "$(hostname)" = "optimizer.cs.umd.edu" ]; then
-    export PATH="${HOME}/bin:$PATH"
+if [ "$(hostname)" = "optimizer.cs.umd.edu" ] || [ "$(hostname)" = "osprey.cs.northwestern.edu" ]; then
     export FZF_BASE="${HOME}/bin/.fzf"
 fi
+export PATH="${HOME}/bin:$PATH"
