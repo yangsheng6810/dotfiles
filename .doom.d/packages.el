0c79ac7dd3a22761aa5f29498d02a100ace7191c
Sheng Yang <yangsheng6810@gmail.com>
2020-10-19 13:53:02 -0500

    Bump org version

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -111,7 +111,7 @@
   :recipe (:type git
            :repo "https://code.orgmode.org/bzg/org-mode"
            :files ("*.el" "lisp/*.el" "contrib/lisp/*.el"))
-  :pin "220f2b0d93a6927eb673978c0042a1d4673e86aa"
+  :pin "8402c4a778136a132833386c817732c58d60c637"
   ;; Prevents built-in Org from sneaking into the byte-compilation of
   ;; `org-plus-contrib', and inform other packages that `org-mode' satisfies the
   ;; `org' dependency: https://github.com/raxod502/straight.el/issues/352
