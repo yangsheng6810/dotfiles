941c24cb9224541687ea8dc70447af650d731ea9
Sheng Yang <yangsheng6810@gmail.com>
2022-02-03 18:25:09 -0600

    Try to fix omz update message

--- .shell/env.sh
+++ .shell/env.sh
@@ -34,6 +34,3 @@ export MAKEFLAGS="-j$(nproc)"
 if ! command pacman 2> /dev/null; then
     PATH="$PATH:$HOME/bin/binary"
 fi
-# disable oh-my-zsh auto update
-export DISABLE_UPDATE_PROMPT=true
-export DISABLE_AUTO_UPDATE=true
--- .zprofile
+++ .zprofile
@@ -7,3 +7,7 @@ export LC_CTYPE=zh_CN.UTF-8
 
 
 export PATH="$HOME/bin:$HOME/.cargo/bin:$PATH"
+
+# disable oh-my-zsh auto update
+export DISABLE_UPDATE_PROMPT=true
+export DISABLE_AUTO_UPDATE=true
