71481dcdb5669a683266e06276430592db1e3f8c
Sheng Yang <styang@fastmail.com>
2022-04-25 17:08:29 -0500

    Use HOSTNAME instead of hostname

--- .shell/pre-fix.sh
+++ .shell/pre-fix.sh
@@ -1,7 +1,7 @@
 # This file contains fixes that need to be done before zgen is loaded
 
 # Prepare FZF_BASE for fzf to use on optimizer
-if [ "$(hostname)" = "optimizer.cs.umd.edu" ] || [ "$(hostname)" = "osprey.cs.northwestern.edu" ]; then
+if [ "${HOSTNAME}" = "optimizer.cs.umd.edu" ] || [ "${HOSTNAME}" = "osprey.cs.northwestern.edu" ]; then
     export FZF_BASE="${HOME}/bin/.fzf"
     # Need this for FZF oh-my-zsh module to work
     export PATH="${HOME}/bin:$PATH"
