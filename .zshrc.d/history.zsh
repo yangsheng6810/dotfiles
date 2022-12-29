b01e9b9d382cf7d412edcdb8f345f17681c8c152
Sheng Yang <styang@fastmail.com>
2022-12-29 10:00:38 +0800

    Update zsh history config

--- .zshrc.d/history.zsh
+++ .zshrc.d/history.zsh
@@ -9,7 +9,7 @@ setopt hist_expire_dups_first
 setopt hist_ignore_dups # ignore duplication command history list
 setopt hist_ignore_space
 setopt hist_verify
-setopt inc_append_history
+setopt inc_append_history_time
 unsetopt share_history # share command history data
 
 alias history='omz_history -i'
